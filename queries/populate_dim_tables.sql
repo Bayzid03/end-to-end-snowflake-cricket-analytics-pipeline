d49f448bb5593b7aa09f5ffaae49e73ed788d1e8
Bayzid03 <143407674+Bayzid03@users.noreply.github.com>
2025-11-11 10:20:27 +0600

    Implement player dimension population steps
    Added SQL steps to populate player_dim from curated player data.

--- queries/populate_dim_tables.sql
+++ queries/populate_dim_tables.sql
@@ -31,3 +31,33 @@ order by team_name;
 
 -- Step 3: Verify the populated team_dim
 select * from cricket.enriched.team_dim order by team_name;
+
+
+-- ------------------------------------------------------------
+-- PLAYER DIMENSION
+-- ------------------------------------------------------------
+
+-- Step 1: Preview player data from curated player table
+select * from cricket.curated.player_curated_table limit 10;
+
+-- Step 2: Group players by country and name to remove duplicates
+select country, player_name
+from cricket.curated.player_curated_table
+group by country, player_name;
+
+-- Step 3: Join players with team_dim to get team_id
+select a.country, b.team_id, a.player_name
+from cricket.curated.player_curated_table a
+join cricket.enriched.team_dim b
+    on a.country = b.team_name
+group by a.country, b.team_id, a.player_name;
+
+-- Step 4: Insert into player_dim (team_id + player_name)
+insert into cricket.enriched.player_dim (team_id, player_name)
+select b.team_id, a.player_name
+from cricket.curated.player_curated_table a
+join cricket.enriched.team_dim b
+    on a.country = b.team_name;
+
+-- Step 5: Verify player_dim
+select * from cricket.enriched.player_dim;
