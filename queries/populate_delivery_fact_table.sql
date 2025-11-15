ac5767a0181530dda45104722a9b59dc82d19da6
Bayzid03 <143407674+Bayzid03@users.noreply.github.com>
2025-11-15 22:08:56 +0600

    Create delivery fact table

--- queries/populate_delivery_fact_table.sql
+++ queries/populate_delivery_fact_table.sql
@@ -65,3 +65,27 @@ join player_dim bpd on d.bowler = bpd.player_name
 join player_dim spd on d.batter = spd.player_name
 join player_dim nspd on d.non_striker = nspd.player_name
 where d.match_type_number = 4686;
+
+-- ------------------------------------------------------------
+-- Create Delivery Fact Table
+-- ------------------------------------------------------------
+-- Define delivery_fact with foreign keys linking to match_fact, team_dim, and player_dim.
+CREATE or replace TABLE delivery_fact (
+    match_id INT,
+    team_id INT,
+    bowler_id INT,
+    batter_id INT,
+    non_striker_id INT,
+    over INT,
+    runs INT,
+    extra_runs INT,
+    extra_type VARCHAR(255),
+    player_out VARCHAR(255),
+    player_out_kind VARCHAR(255),
+
+    CONSTRAINT fk_del_match_id FOREIGN KEY (match_id) REFERENCES match_fact (match_id),
+    CONSTRAINT fk_del_team FOREIGN KEY (team_id) REFERENCES team_dim (team_id),
+    CONSTRAINT fk_bowler FOREIGN KEY (bowler_id) REFERENCES player_dim (player_id),
+    CONSTRAINT fk_batter FOREIGN KEY (batter_id) REFERENCES player_dim (player_id),
+    CONSTRAINT fk_striker FOREIGN KEY (non_striker_id) REFERENCES player_dim (player_id)
+);
